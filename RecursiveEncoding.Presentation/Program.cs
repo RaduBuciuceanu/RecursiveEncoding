4e9a09ab17ce78510282d316fb3828ef1e2e80f5
Radu B <radu.buciuceanu@gmail.com>
2019-02-02 13:20:08 +0200

    Search pattern should be inserted by the user.

--- RecursiveEncoding.Presentation/Program.cs
+++ RecursiveEncoding.Presentation/Program.cs
@@ -7,22 +7,43 @@ namespace RecursiveEncoding.Presentation
 {
     public static class Program
     {
+        private static string path;
+        private static string searchPattern;
+
         public static void Main(string[] args)
         {
-            string path = Console.ReadLine();
-            IEnumerable<string> files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+            path = ReadDirectoryPath();
+            searchPattern = ReadSearchPattern();
 
+            IEnumerable<string> files = Directory.GetFiles(path, $"*{searchPattern}", SearchOption.AllDirectories);
+            ChangeEncoding(files);
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+
+        private static string ReadDirectoryPath()
+        {
+            Console.Write("Directory path: ");
+            return Console.ReadLine();
+        }
+
+        private static string ReadSearchPattern()
+        {
+            Console.WriteLine("Search pattern like .cs or .txt (only a single one is supported):");
+            return Console.ReadLine();
+        }
+
+        private static void ChangeEncoding(IEnumerable<string> files)
+        {
             foreach (string file in files)
             {
-                if (file.EndsWith(".cs") || file.EndsWith(".json"))
+                if (file.EndsWith(searchPattern))
                 {
                     File.WriteAllText(file, File.ReadAllText(file) + Environment.NewLine, new UTF8Encoding(true));
                     Console.WriteLine($"{file} was converted.");
                 }
             }
-
-            Console.WriteLine("Done.");
-            Console.ReadKey();
         }
     }
 }

3cb477e283ffe9507540234dfce5a6ebf65f60e3
Radu B <radu.buciuceanu@gmail.com>
2019-02-02 13:13:43 +0200

    Initial project logic was implemented.

---
+++ RecursiveEncoding.Presentation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RecursiveEncoding.Presentation
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            IEnumerable<string> files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                if (file.EndsWith(".cs") || file.EndsWith(".json"))
+                {
+                    File.WriteAllText(file, File.ReadAllText(file) + Environment.NewLine, new UTF8Encoding(true));
+                    Console.WriteLine($"{file} was converted.");
+                }
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
